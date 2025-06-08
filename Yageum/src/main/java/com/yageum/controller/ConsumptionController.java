1b2ad7dc64fc6695028e9dac1ff4af3f6c1da016
dygks <dygks@JYH>
2025-06-08 13:36:36 +0900

    css 분리 , 맵핑, 해더-풋터 연결

---
+++ Yageum/src/main/java/com/yageum/controller/ConsumptionController.java
@@ -0,0 +1,34 @@
+package com.yageum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@Controller
+@RequiredArgsConstructor
+@Log
+@RequestMapping("/consumption/*")
+public class ConsumptionController {
+	
+	@GetMapping("/eanalysis")
+	public String eanalysis() {
+		log.info("ConsumptionController eanalysis()");
+		return "/consumption/expense_analysis";
+	}
+	
+	@GetMapping("/efeedback")
+	public String efeedback() {
+		log.info("ConsumptionController efeed()");
+		return "/consumption/expense_feedback";
+	}
+	
+	@GetMapping("/canalysis")
+	public String canalysis() {
+		log.info("ConsumptionController canalysis()");
+		return "/consumption/consumption_analysis";
+	}
+	
+}
