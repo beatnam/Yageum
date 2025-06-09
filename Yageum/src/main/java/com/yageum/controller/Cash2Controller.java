323868bc93ed32826aeb885fbb0c5aba691c96c1
gts5045gts <127909480+gts5045gts@users.noreply.github.com>
2025-06-09 12:12:42 +0900

    chart / quest 파일 이름 바꿔서 폴더에 넣고

---
+++ Yageum/src/main/java/com/yageum/controller/Cash2Controller.java
@@ -0,0 +1,30 @@
+package com.yageum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+@Controller
+@Log
+@RequiredArgsConstructor
+@RequestMapping("/cashbook/*")
+public class Cash2Controller {
+
+	@GetMapping("/chart")
+	public String chart() {
+		log.info("Cash2Controller chart()");
+		
+		
+		return "/cashbook/cashbook_chart";
+	}
+	@GetMapping("/quest")
+	public String quest() {
+		log.info("Cash2Controller quest()");
+		
+		
+		return "/cashbook/cashbook_quest";
+	}
+}
