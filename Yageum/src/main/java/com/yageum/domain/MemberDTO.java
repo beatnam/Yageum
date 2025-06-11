b11fd998e30ae2b717b21ef085af3167653f1ae8
beatnam <bnk815@gmail.com>
2025-06-11 13:13:26 +0900

    암호화 제외한 회원가입 완료

---
+++ Yageum/src/main/java/com/yageum/domain/MemberDTO.java
@@ -0,0 +1,33 @@
+package com.yageum.domain;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class MemberDTO {
+
+	private int memberIn;
+	private String memberLevel;
+	private String memberId;
+	private String memberPasswd;
+	private String memberRepasswd;
+	private String memberName;
+	private LocalDate memberBirth;
+	private String memberEmail;
+	private String memberGender;
+	private String memberPhone;
+	private String memberAddress;
+	private LocalDate createDate;
+	private LocalDate updateDate;
+	private boolean memberConsent;
+	private boolean emailConsent;
+	private String memberState;
+	private int memberReward;
+	private boolean memberIsFirst;
+
+}
