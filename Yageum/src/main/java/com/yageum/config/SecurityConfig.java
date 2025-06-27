69b4c845dacceec046a16c5d9edf97d090eab1fb
gts5045gts <127909480+gts5045gts@users.noreply.github.com>
2025-06-27 11:33:41 +0900

    재설정

--- Yageum/src/main/java/com/yageum/config/SecurityConfig.java
+++ Yageum/src/main/java/com/yageum/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .requestMatchers("/consumption/**", "/eanalysis/**", "/efeedback/**", "/canalysis/**", "/bplanner/**").hasAnyRole("USER", "ADMIN")
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/admin/**","/user_detail/**","/authority/**").hasRole("ADMIN")
-                .requestMatchers("/notice/**").hasRole("USER")
+                .requestMatchers("/notice/**").hasAnyRole("USER","ADMIN")
                 .anyRequest().authenticated()
             )
             .securityContext(security -> security
