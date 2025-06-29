6d54d7b49bf93224d425fa4a0f1c0741b1821e06
Lucy <Lucy@192.168.219.223>
2025-06-29 20:00:01 +0900

    250629_주현_마이페이지수단완성

---
+++ Yageum/src/main/java/com/yageum/repository/CardCorporationRepository.java
@@ -0,0 +1,16 @@
+package com.yageum.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yageum.entity.CardCompany;
+
+
+
+
+
+public interface CardCorporationRepository extends JpaRepository<CardCompany, Integer> {
+
+	
+}
